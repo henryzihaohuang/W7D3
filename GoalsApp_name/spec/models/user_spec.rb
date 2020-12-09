db9f006e09601fafff3664f5ed8bc7b11118157e
henryzihaohuang <atlas.bb.contact@gmail.com>
2020-12-09 13:44:49 -0600

    Write user_spec.rb

--- GoalsApp_name/spec/models/user_spec.rb
+++ GoalsApp_name/spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  subject(:user) do
+    FactoryBot.build(:user, 
+    username:"John", 
+    password:"asdfasdf")
+
+  end
+
+  it { should validate_presence_of(:username)}
+  it { should validate_presence_of(:password_digest)}
+  it { should validate_presence_of(:session_token)}
+  it { should validate_length_of(:password).is_at_least(6)}
+
+  describe "uniqueness" do
+    before(:each) do
+      create(:user)
+    end
+
+    it {should validate_uniqueness_of(:username)}
+    it {should validate_uniqueness_of(:session_token)}
+  end
+
+  describe "::find_by_credentials" do
+    
+    it "should return a user if valid credentials" do
+      expect(User.find_by_credentials("John", 
+      "asdfasdf")).to eq(user)
+    end
+      
+    it "should return nil if invalid credentials" do
+      expect(User.find_by_credentials("John", 
+      "a")).to eq(nil)
+    end
+  end
+
+
 end
